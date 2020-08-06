7abccf62df4528fbcd4b610e638a2b34356b094f
sylvain kadjo <sylvain@MacBook-Pro-de-SEJEN.local>
2020-08-06 09:01:08 +0000

    Learn how to customize django admin

--- src/authentication/admin.py
+++ src/authentication/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
 
-# Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Account', {'fields': ['username', 'email', 'password']}),
+        ('Indentification', {'fieds': ['first_name', 'last_name', 'email']}),
+    ]
+
+
+admin.site.register(User, UserAdmin)
--- src/todo/admin.py
+++ src/todo/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from .models import Todo
 
+class TodoAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Info', {'fields': ['title']}),
+        ('Test', {'fields': ['is_conpleted']})
+    ]
 
-admin.site.register(Todo)
+    list_display = ('title', 'is_conpleted')
+    list_filter = ['is_conpleted']
+    search_fields = ['title', 'is_conpleted']
+    # fields = ['title', 'is_conpleted']
+
+
+admin.site.register(Todo, TodoAdmin)
